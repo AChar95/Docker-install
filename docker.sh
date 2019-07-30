82b9112fd4a07b581466454928c227e28426df8c
Allan Charles <50716770+AChar95@users.noreply.github.com>
2019-07-30 16:50:43 +0100

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -3,9 +3,7 @@
 echo "***************************** installing ***************************************"
 sudo apt-get update
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+curl https://get.docker.com | sudo bash
 sudo groupadd docker
 sudo usermod -aG jenkins docker
 sudo gpasswd -a jenkins docker
